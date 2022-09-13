d7caf157be6b107eb21480e19e5329aef4c58179
ConsueloCabezas <113408973+ConsueloCabezas@users.noreply.github.com>
2022-09-13 17:16:26 -0600

    Ejercicios del periodo I, II y III
    Consuelo Cabezas 2A #11

---
+++ Unidad3-Semana4-Clase11-Ejercicio1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int number = 0;
+	int sum = 0;
+	do{
+		sum += number;
+		cout << "Ingrese un numero: ";
+		cin >> number;
+	} while (number >= 0);
+	cout << "\nLa suma es " << sum << endl;
+	return 0;
+}
